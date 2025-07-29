b99375e20c9742a61b679e12a97ad5baa548e5bc
danichagas <chagasdaniel788@gmail.com>
2025-07-29 00:15:41 -0300

    feat: points update route

--- src/controllers/PointController.ts
+++ src/controllers/PointController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 import { validationResult } from 'express-validator'
-import { SetOfPoints } from '../models/Point.js'
+import { SetOfPoints, type IPoint } from '../models/Point.js'
 
 //POST
 export const createSetOfPoints = async (req: Request, res: Response) => {
@@ -35,5 +35,39 @@ export const getSetOfPoints = async (req: Request, res: Response) => {
     res.status(200).json(set)
   } catch (error) {
     res.status(500).json({ messgae: 'Erro no servidor', error })
+  }
+}
+
+//PATCH /:id
+export const updateSetOfPoints = async (req: Request, res: Response) => {
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  try {
+    const set = await SetOfPoints.findById(req.params.id)
+    if (!set) {
+      return res.status(404).json({ message: 'Os pontos não foram encontrados' })
+    }
+
+    const pointsToUpdate = req.body.points
+    
+    for (const updatedPoint of pointsToUpdate) {
+      const existingPointIndex = set.points.findIndex(p => p.name === updatedPoint.name)
+
+      if (existingPointIndex !== -1) {
+        set.points[existingPointIndex].x = updatedPoint.x
+        set.points[existingPointIndex].y = updatedPoint.y
+      } else {
+        set.points.push(updatedPoint)
+      }
+    }
+
+    await set.save()
+    res.status(200).json(set)
+  } catch (error) {
+    res.status(500).json({ message: 'Erro no servidor', error })
   }
 }
