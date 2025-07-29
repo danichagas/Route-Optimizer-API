5b5c3b891cbf3a214b5f05c7675637c8ce154d93
danichagas <chagasdaniel788@gmail.com>
2025-07-29 14:22:29 -0300

    feat: performing validations with express-validator

---
+++ src/routes/PointRoute.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+import { body } from 'express-validator'
+import { createSetOfPoints, getSetOfPoints, updateSetOfPoints } from '../controllers/PointController.js'
+
+const router = Router()
+
+const pointValidationRules = [
+  body('points').isArray({ min: 1 }).withMessage('Os pontos não pode ser um array vazio!'),
+  body('points.*.name').notEmpty().withMessage('O campo "nome" é obrigatório!'),
+  body('points.*.x').isNumeric().withMessage('A coordenada "x" deve ser um número!'),
+  body('points.*.y').isNumeric().withMessage('A coordenada "y" deve ser um número!')
+]
+
