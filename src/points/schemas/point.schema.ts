4d0a3b0a2669a8ccf8418f57aa8b00b55e85227f
danichagas <chagasdaniel788@gmail.com>
2025-08-23 14:46:54 -0300

    feat: create point schema

---
+++ src/points/schemas/point.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { Types, Document } from 'mongoose'
+
+@Schema()
+export class Point extends Document {
+
+    @Prop({ required: true })
+    x: number
+
+    @Prop({ required: true })
+    y: number
+}
+
+export const PointSchema = SchemaFactory.createForClass(Point)
+
+@Schema({ timestamps: true })
+export class SetOfPoints extends Document {
+    @Prop([PointSchema])
+    points: Point[]
+}
+
+export const SetOfPointsSchema = SchemaFactory.createForClass(SetOfPoints)
