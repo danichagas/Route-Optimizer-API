95cf896db08635fd8a91a91b3f8acaf46a34173c
danichagas <chagasdaniel788@gmail.com>
2025-08-23 14:19:45 -0300

    feat: generate module points and routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PointsModule } from './points/points.module';
+import { RouteModule } from './route/route.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://chagas:dani@routeoptimizer.nttzkbn.mongodb.net/?retryWrites=true&w=majority&appName=RouteOptimizer')
+    MongooseModule.forRoot('mongodb+srv://chagas:dani@routeoptimizer.nttzkbn.mongodb.net/?retryWrites=true&w=majority&appName=RouteOptimizer'),
+    PointsModule,
+    RouteModule
   ],
   controllers: [AppController],
   providers: [AppService],
