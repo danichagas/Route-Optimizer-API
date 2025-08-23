09505cdb4668f437a3c485ae549d76ec92a6e957
danichagas <chagasdaniel788@gmail.com>
2025-08-22 22:52:14 -0300

    fix: connecting to MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://chagas:dani@routeoptimizer.nttzkbn.mongodb.net/?retryWrites=true&w=majority&appName=RouteOptimizer')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
