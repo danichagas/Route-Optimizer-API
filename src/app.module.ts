c38a878c78e5ac3cd3f9cec18c7adf9e226d2b4e
danichagas <chagasdaniel788@gmail.com>
2025-08-25 19:19:50 -0300

    feat: configuring throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,15 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { PointsModule } from './points/points.module'
 import { RouteModule } from './route/route.module'
+import { ThrottlerModule } from '@nestjs/throttler'
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://chagas:dani@routeoptimizer.nttzkbn.mongodb.net/?retryWrites=true&w=majority&appName=RouteOptimizer'),
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 20,
+    }]),
     PointsModule,
     RouteModule
   ],
