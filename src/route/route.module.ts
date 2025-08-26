ec30959398a1819e97a0ce5e1622fa261a315662
danichagas <chagasdaniel788@gmail.com>
2025-08-26 17:13:20 -0300

    feat: adding route contoller

--- src/route/route.module.ts
+++ src/route/route.module.ts
@@ -3,6 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { PointsModule } from 'src/points/points.module'
 import { Route, RouteSchema } from './schemas/route.schema'
 import { RoutesService } from './services/routes.service'
+import { RouteController } from './controllers/route.controller';
 
 
 @Module({
@@ -11,5 +12,6 @@ import { RoutesService } from './services/routes.service'
         MongooseModule.forFeature([{ name: Route.name, schema: RouteSchema }]),
     ],
     providers: [RoutesService],
+    controllers: [RouteController],
 })
 export class RouteModule {}
